d0b28f2412e5f7c9549a7487f9ca6bf099d7166b
WilliamGoss <WilliamTGoss@gmail.com>
2016-01-28 16:48:20 -0600

    Storage for results and version id
    Everything will be stored in a LinkedHashMap to ensure it is saved in
    insertion order.

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,7 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 
@@ -13,12 +13,26 @@ public class Program {
 		List<String> softwareVersions = new ArrayList<String>();
 		softwareVersions = populateVersionNumber.populateNumbers();
 		
+		//A set to hold the versions and result for each.
+		LinkedHashMap<String, List<ResultObject>> versionResults = new LinkedHashMap<String, List<ResultObject>>();
+		
+		
 		for(String versionID: softwareVersions)
 		{
 			List<ResultObject> results = new ArrayList<ResultObject>();
 			String fileLoc = "../TLE_Scenario_Checker/" + versionID + ".txt";
-			//results = generateResults.parseResults("C:\\Users\\wgoss2\\Documents\\Mona\\TLE_Scenario_Checker\\cassandra-cassandra-1.1.12.txt");
 			results = generateResults.parseResults(fileLoc);
+			
+			//Need to isolate the version id from the string
+			String[] versionSplit = versionID.split("-");
+			String version =  "";
+			if (versionSplit.length > 3) { version = versionSplit[2] + "-" + versionSplit[3]; }
+			else version = versionSplit[2];
+			
+			versionResults.put(version, results);
 		}
+		
+		//versionResults contains a map of all the version numbers and their results
+		//Now it needs to be used in creating graphviz outputs!
 	}
 }
